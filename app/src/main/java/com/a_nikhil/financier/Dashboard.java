0578288521fb1d15668596afcdca4c3fdf737663
Nikhil Anand <nikhilanand1006@gmail.com>
2020-02-09 02:52:12 +0530

    Implemented a Navigation Bar

--- app/src/main/java/com/a_nikhil/financier/Dashboard.java
+++ app/src/main/java/com/a_nikhil/financier/Dashboard.java
@@ -1,14 +1,19 @@
 package com.a_nikhil.financier;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-public class Dashboard extends AppCompatActivity {
+import com.google.android.material.navigation.NavigationView;
+
+public class Dashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -21,13 +26,50 @@ public class Dashboard extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         drawer = findViewById(R.id.dashboard_drawer_layout);
-//        drawer.setStatusBarBackgroundColor(getResources().getColor(R.color.accentDark));
+        NavigationView navigationView = findViewById(R.id.dash_nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.dashboard_nav_drawer_open,
                 R.string.dashboard_nav_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new DashboardFragment()).commit();
+        }
+        navigationView.setCheckedItem(R.id.menu_dashboard);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.menu_dashboard:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new DashboardFragment()).commit();
+                break;
+            case R.id.menu_add_new:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new NewExpenditureFragment()).commit();
+                break;
+            case R.id.menu_visualize:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new VisualizeFragment()).commit();
+                break;
+            case R.id.menu_predict:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new PredictFragment()).commit();
+                break;
+            case R.id.cloud_download:
+                Toast.makeText(getApplicationContext(), "Downloaded from cloud", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.cloud_upload:
+                Toast.makeText(getApplicationContext(), "Uploaded to cloud", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 
     @Override
---
+++ app/src/main/java/com/a_nikhil/financier/NewExpenditureFragment.java
@@ -0,0 +1,19 @@
+package com.a_nikhil.financier;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class NewExpenditureFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_new_expenditure, container, false);
+    }
+}
