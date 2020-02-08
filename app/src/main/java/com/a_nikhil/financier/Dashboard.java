a57af7214c3017210fae0aa1d835da40b8e93dc2
Nikhil Anand <nikhilanand1006@gmail.com>
2020-02-08 16:23:54 +0530

    Test #1

--- app/src/main/java/com/a_nikhil/financier/Dashboard.java
+++ app/src/main/java/com/a_nikhil/financier/Dashboard.java
@@ -10,5 +10,6 @@ public class Dashboard extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+        System.out.println("Hello Dashboard");
     }
 }
