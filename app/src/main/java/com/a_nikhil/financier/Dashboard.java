b5995c065ce04a24691c9a7d48a55f1fa9203829
Nikhil Anand <nikhilanand1006@gmail.com>
2020-02-09 02:18:23 +0530

    Made a Navigation Drawer

--- app/src/main/java/com/a_nikhil/financier/Dashboard.java
+++ app/src/main/java/com/a_nikhil/financier/Dashboard.java
@@ -1,14 +1,41 @@
 package com.a_nikhil.financier;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
 public class Dashboard extends AppCompatActivity {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.dashboard_drawer_layout);
+//        drawer.setStatusBarBackgroundColor(getResources().getColor(R.color.accentDark));
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.dashboard_nav_drawer_open,
+                R.string.dashboard_nav_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
