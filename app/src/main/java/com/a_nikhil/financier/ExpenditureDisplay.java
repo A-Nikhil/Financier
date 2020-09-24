9d1291c5482aa357a9faebc1740c5152ad0aecc0
Nikhil Anand <nikhilanand1006@gmail.com>
2020-09-25 03:52:43 +0530

    Starting with Visualizations

--- app/src/main/java/com/a_nikhil/financier/ExpenditureDisplay.java
+++ app/src/main/java/com/a_nikhil/financier/ExpenditureDisplay.java
@@ -5,10 +5,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.Html;
 import android.view.MenuItem;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.SingleValueDataSet;
+import com.anychart.charts.CircularGauge;
+import com.anychart.enums.Anchor;
+import com.anychart.graphics.vector.text.HAlign;
 
 import java.util.Objects;
 
@@ -42,11 +48,17 @@ public class ExpenditureDisplay extends AppCompatActivity {
                 "This comes under the category<br/>of <b>" + category + "</b>", flag));
         ((TextView) findViewById(R.id.singleExpenditureDate)).setText(Html.fromHtml(
                 "on <b>" + date + "</b>", flag));
-        ((ProgressBar) findViewById(R.id.singleExpenditureBar)).setProgress((int) (amount * 100 / maxIncome), true);
-        ((TextView) findViewById(R.id.singleExpenditurePercentage)).setText(Html.fromHtml(
-                "<b>" + (int) (amount * 100 / maxIncome) + " %</b>", flag));
-        ((TextView) findViewById(R.id.singleExpenditureTotalIncome)).setText(Html.fromHtml(
-                "of your total monthly income of \u20B9 " + maxIncome, flag));
+
+        // Visualization starting here
+        AnyChartView anyChartView = findViewById(R.id.anyChartView);
+        anyChartView.setChart(setSpeedometer(maxIncome, amount));
+
+        // fixme changes
+//        ((ProgressBar) findViewById(R.id.singleExpenditureBar)).setProgress((int) (amount * 100 / maxIncome), true);
+//        ((TextView) findViewById(R.id.singleExpenditurePercentage)).setText(Html.fromHtml(
+//                "<b>" + (int) (amount * 100 / maxIncome) + " %</b>", flag));
+//        ((TextView) findViewById(R.id.singleExpenditureTotalIncome)).setText(Html.fromHtml(
+//                "of your total monthly income of \u20B9 " + maxIncome, flag));
 
     }
 
@@ -54,5 +66,98 @@ public class ExpenditureDisplay extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         startActivity(new Intent(ExpenditureDisplay.this, Dashboard.class));
         return super.onOptionsItemSelected(item);
+    }
+
+    private CircularGauge setSpeedometer(double maxIncome, double amount) {
+        CircularGauge circularGauge = AnyChart.circular();
+
+        // Making the gauge
+        circularGauge
+                .stroke(null)
+                .startAngle(0)
+                .sweepAngle(360);
+
+        // Setting data
+        double percentage = amount * 100 / maxIncome;
+        percentage = (double) Math.round(percentage * 100) / 100;
+        circularGauge.data(new SingleValueDataSet(new Double[]{percentage}));
+
+        // Axes
+        circularGauge.axis(0)
+                .startAngle(-150)
+                .radius(80)
+                .sweepAngle(300)
+                .width(3)
+                .ticks("{type:'line', length:4, position:'outside'}");
+
+        circularGauge.axis(0).labels().position("outside");
+
+        circularGauge.axis(0).scale()
+                .minimum(0)
+                .maximum(100);
+
+        circularGauge.axis(0).scale()
+                .ticks("{interval: 10}")
+                .minorTicks("{interval: 10}");
+
+        circularGauge.needle(0)
+                .stroke(null)
+                .startRadius("6%")
+                .endRadius("80%")
+                .startWidth("2%")
+                .endWidth(0);
+
+        circularGauge.cap()
+                .radius("4%")
+                .enabled(true)
+                .stroke(null);
+
+        circularGauge.label(0)
+                .text("<span style=\"font-size: 25\">" + amount + "</span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER);
+        circularGauge.label(0)
+                .anchor(Anchor.CENTER_TOP)
+                .offsetY(100)
+                .padding(15, 20, 0, 0);
+
+        circularGauge.label(1)
+                .text("<span style=\"font-size: 20\">" + percentage + "% </span>")
+                .useHtml(true)
+                .hAlign(HAlign.CENTER);
+        circularGauge.label(1)
+                .anchor(Anchor.CENTER_TOP)
+                .offsetY(-100)
+                .padding(5, 10, 0, 0)
+                .background("{fill: 'none', stroke: '#c1c1c1', corners: 3, cornerType: 'ROUND'}");
+
+        circularGauge.range(0,
+                "{\n" +
+                        "    from: 0,\n" +
+                        "    to: 20,\n" +
+                        "    position: 'inside',\n" +
+                        "    fill: 'green 0.5',\n" +
+                        "    stroke: '1 #000',\n" +
+                        "    startSize: 6,\n" +
+                        "    endSize: 6,\n" +
+                        "    radius: 80,\n" +
+                        "    zIndex: 1\n" +
+                        "  }");
+
+        circularGauge.range(1,
+                "{\n" +
+                        "    from: 80,\n" +
+                        "    to: 100,\n" +
+                        "    position: 'inside',\n" +
+                        "    fill: 'red 0.5',\n" +
+                        "    stroke: '1 #000',\n" +
+                        "    startSize: 6,\n" +
+                        "    endSize: 6,\n" +
+                        "    radius: 80,\n" +
+                        "    zIndex: 1\n" +
+                        "  }");
+
+
+        return circularGauge;
     }
 }
