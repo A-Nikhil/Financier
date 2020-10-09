d189bd44fac713d7900b41df9e20cfd5f9883e32
Nikhil Anand <nikhilanand1006@gmail.com>
2020-10-10 02:29:38 +0530

    Expenditure UI done

--- app/src/main/java/com/a_nikhil/financier/NewExpenditureActivity.java
+++ app/src/main/java/com/a_nikhil/financier/NewExpenditureActivity.java
@@ -1,28 +1,117 @@
 package com.a_nikhil.financier;
 
+import android.app.DatePickerDialog;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.view.ViewCompat;
 
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
+import com.google.android.material.snackbar.Snackbar;
+
+import java.util.Calendar;
 
 public class NewExpenditureActivity extends AppCompatActivity {
+
+    private static final String TAG = "NewExpenditureActivity";
+
+    EditText dateEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_expenditure);
-        final ChipGroup chipGroup = findViewById(R.id.CategoriesChipGroup);
-        findViewById(R.id.addNewExpenditureActivity).setOnClickListener(new View.OnClickListener() {
+        dateEditText = findViewById(R.id.NewExpenditureActivityDate);
+
+        // Adding a DatePicker
+        final Calendar c = Calendar.getInstance();
+        dateEditText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
-            public void onClick(View v) {
-                int id = chipGroup.getCheckedChipId();
-                Chip chip = findViewById(id);
-                Toast.makeText(NewExpenditureActivity.this, "Chip selected : " + chip.getText(), Toast.LENGTH_SHORT).show();
+            public void onFocusChange(View view, boolean hasFocus) {
+                int mYear, mMonth, mDay;
+                if (hasFocus) {
+                    mYear = c.get(Calendar.YEAR);
+                    mMonth = c.get(Calendar.MONTH);
+                    mDay = c.get(Calendar.DAY_OF_MONTH);
+                    DatePickerDialog datePickerDialog = new DatePickerDialog(NewExpenditureActivity.this, new DatePickerDialog.OnDateSetListener() {
+                        @Override
+                        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                            String dater = dayOfMonth + "-" + (monthOfYear + 1) + "-" + year;
+                            dateEditText.setText(dater);
+                        }
+                    }, mYear, mMonth, mDay);
+                    datePickerDialog.show();
+                } else {
+                    dateEditText.clearFocus();
+                }
             }
         });
+    }
+
+    public void addNewExpenditure(View v) {
+        final EditText nameEditText = findViewById(R.id.NewExpenditureActivityName);
+        final EditText amountEditText = findViewById(R.id.NewExpenditureActivityAmount);
+        final Chip selectedCategory = findViewById(((ChipGroup) findViewById(R.id.CategoriesChipGroup)).getCheckedChipId());
+
+        final String name = nameEditText.getText().toString();
+        final String amount = amountEditText.getText().toString();
+        final String date = dateEditText.getText().toString();
+        final String category = selectedCategory.getText().toString();
+
+        if (performValidation(name, amount, date, category, v)) {
+            showStatusAsSnackbar("Expenditure Added", v);
+        }
+    }
+
+    private boolean performValidation(String name, String amount, String date, String category, View v) {
+        try {
+            if (name == null || name.length() == 0) {
+                throw new Exception("Please enter a valid title");
+            } else if (Double.parseDouble(amount) <= 0.0) {
+                throw new Exception("Please enter a valid amount");
+            } else if (category.equals("Select a category..")) {
+                throw new Exception("Select a Category");
+            } else if (date == null) {
+                throw new Exception("Enter a valid date");
+            }
+            return true;
+        } catch (Exception e) {
+            Log.d(TAG, "performValidation: " + e.getMessage());
+            showStatusAsSnackbar(e.getMessage(), v);
+            return false;
+        }
+    }
+
+    private void showStatusAsSnackbar(String status, View v) {
+        final Snackbar infoSnackbar = Snackbar.make(v, status, Snackbar.LENGTH_SHORT);
+        infoSnackbar.setAction("Dismiss", new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                infoSnackbar.dismiss();
+            }
+        });
+
+        // Setting elevation of snackbar
+        infoSnackbar.getView().setFitsSystemWindows(false);
+        ViewCompat.setOnApplyWindowInsetsListener(infoSnackbar.getView(), null);
+
+        // Setting color of Dismiss text
+        infoSnackbar.setActionTextColor(ContextCompat.getColor(getApplicationContext(), R.color.accent));
+
+        // Setting snackbar text color
+        ((TextView) infoSnackbar.getView().findViewById(R.id.snackbar_text)).setTextColor(Color.WHITE);
+        infoSnackbar.show();
+    }
+
+    public void cancelActivity(View v) {
+
     }
 }
