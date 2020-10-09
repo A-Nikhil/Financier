874dc78f1859a67a2b32eacd72fd4d00c95a4bdb
Nikhil Anand <nikhilanand1006@gmail.com>
2020-10-09 19:40:42 +0530

    ChipGroupWorks

--- app/src/main/java/com/a_nikhil/financier/NewExpenditureActivity.java
+++ app/src/main/java/com/a_nikhil/financier/NewExpenditureActivity.java
@@ -1,8 +1,13 @@
 package com.a_nikhil.financier;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipGroup;
 
 public class NewExpenditureActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class NewExpenditureActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_expenditure);
+        final ChipGroup chipGroup = findViewById(R.id.chipGroup);
+        findViewById(R.id.button3).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int id = chipGroup.getCheckedChipId();
+                Chip chip = findViewById(id);
+                Toast.makeText(NewExpenditureActivity.this, "Chip selected : " + chip.getText(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
