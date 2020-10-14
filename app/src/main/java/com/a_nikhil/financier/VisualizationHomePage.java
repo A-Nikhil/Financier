f8009049a7cd2972b127c49abeed17071b6a7313
Nikhil Anand <nikhilanand1006@gmail.com>
2020-10-15 03:49:30 +0530

    Working on menus and fragments

--- app/src/main/java/com/a_nikhil/financier/VisualizationHomePage.java
+++ app/src/main/java/com/a_nikhil/financier/VisualizationHomePage.java
@@ -6,12 +6,16 @@ import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.a_nikhil.financier.commons.AndroidUtilities;
 import com.google.android.material.navigation.NavigationView;
 
 public class VisualizationHomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -25,13 +29,32 @@ public class VisualizationHomePage extends AppCompatActivity implements Navigati
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visualization_home_page);
 
+        Toolbar toolbar = findViewById(R.id.vis_toolbar);
+        setSupportActionBar(toolbar);
+
         drawer = findViewById(R.id.visualization_drawer_layout);
         NavigationView vizNav = findViewById(R.id.dash_vis_nav);
         View vizHeader = vizNav.getHeaderView(0);
+        vizNav.setNavigationItemSelectedListener(this);
 
+//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+//                R.string.dashboard_nav_drawer_open,
+//                R.string.dashboard_nav_drawer_close);
+//        drawer.addDrawerListener(toggle);
+//        toggle.syncState();
+
+        /*
         // Get Bundle
         Bundle inputBundle = getIntent().getExtras();
         assert inputBundle != null;
+         */
+
+//        /* Uncomment when testing is done
+        Bundle inputBundle = new Bundle();
+        inputBundle.putString("name", "Covid");
+        inputBundle.putString("maxIncome", "25000");
+        inputBundle.putString("email", "covid19@pandemic.org");
+//         */
 
         // Get name from Bundle
         String name = inputBundle.getString("name");
@@ -50,7 +73,34 @@ public class VisualizationHomePage extends AppCompatActivity implements Navigati
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        AndroidUtilities.ShowStatusAsSnackbar snackbar = new AndroidUtilities.ShowStatusAsSnackbar(getApplicationContext(), drawer);
+        switch (item.getItemId()) {
+            case R.id.categories_vs_total:
+                snackbar.showStatus("A");
+                break;
+            case R.id.categories_vs_total1:
+                snackbar.showStatus("B");
+                break;
+            case R.id.categories_vs_total2:
+                snackbar.showStatus("C");
+                break;
+            case R.id.categories_vs_total3:
+                snackbar.showStatus("D");
+                break;
+            default:
+                Toast.makeText(this, "default", Toast.LENGTH_SHORT).show();
+        }
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
 }
